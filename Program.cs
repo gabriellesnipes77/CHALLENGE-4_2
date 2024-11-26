a4c29212f8eaff272e0ca2e6ee0ef0a424b11b5e
BIG DON <BIG DON@MSI>
2024-11-26 13:44:57 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ASSIGNMENT_4_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> count = new Dictionary<string, int>();
+
+            string[] input = ["a", "b", "c", "a", "c"];
+
+            foreach (string s in input)
+            {
+                if (!count.TryAdd(s, 1))
+                {
+                    count[s]++;
+                }
+            }
+       
+
+                Console.WriteLine("\nUpdate Dictionary:");
+                foreach (KeyValuePair<string, int> kvp in count)
+                {
+                    Console.WriteLine("Key = {0}, Count = {1}", 
+                        kvp.Key, kvp.Value);
+                }
+            }
+        }
+    }
+
